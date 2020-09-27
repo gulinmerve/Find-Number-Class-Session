3075df194cea05149b540ac9f380f1567e5c0205
Merve <gulineyel@gmail.com>
2020-09-27 17:24:00 +0300

    second lecture started

--- index.js
+++ index.js
@@ -10,18 +10,25 @@ let guess_count = 0;
 const random_number = Math.floor(Math.random() * 100 + 1);
 console.log(random_number);
 document.querySelector("#check_it").addEventListener("click", checkNumber);
+
 function checkNumber() {
-    guess_count++;
-    // Input elementine tekrar tekrar daha rahat erişim sağlamak için
-    let userInputElement = document.querySelector("#nr_game");
-    document.querySelector("#count_it").innerText = `Guess count is: ${guess_count}`;
-    if (userInputElement.value == random_number) {
-        alert("You are the winner!");
-    } else if (userInputElement.value > random_number) {
-        document.querySelector("#answer").innerText = "Try a smaller number!"
-    } else if (userInputElement.value < random_number) {
-        document.querySelector("#answer").innerText = "Try a higher number!"
-    } else if (isNaN(userInputElement.value)) {
-        document.querySelector("#answer").innerText = "It's not a number!"
-    }
+  guess_count++;
+  // Input elementine tekrar tekrar daha rahat erişim sağlamak için
+  let userInputElement = document.querySelector("#nr_game");
+  let answerLabelElement = document.querySelector("#answer");
+  document.querySelector(
+    "#count_it"
+  ).innerText = `Guess count is: ${guess_count}`;
+
+  if (userInputElement.value == random_number) {
+    alert("You are the winner!");
+  } else if (userInputElement.value > random_number) {
+    answerLabelElement.innerText = "Try a smaller number!";
+  } else if (userInputElement.value < random_number) {
+    answerLabelElement.innerText = "Try a higher number!";
+  } else if (isNaN(userInputElement.value)) {
+    answerLabelElement.innerText = "It's not a number!";
+  }  else if (userInputElement.value == "") {
+    answerLabelElement.innerText = "Enter a Number";
+  }
 }
